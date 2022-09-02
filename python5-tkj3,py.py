8f0028f24c2be866263bc57d56bc7eee038e3762
TIANNIRON <112367756+TIANNIRON@users.noreply.github.com>
2022-09-01 20:18:10 -0700

    Add files via upload

---
+++ python5-tkj3,py.py
@@ -0,0 +1,29 @@
+sisi1=10
+sisi2=10
+luas_p= sisi1*sisi2
+
+print("Luas persegi adalah", luas_p)
+
+
+alas =20
+tinggi =10
+luas_s= alas * tinggi / 2
+
+print("Luas segita adalah", luas_s)
+
+alas=10
+tinggi=20
+luas_j=alas * tinggi / 2
+
+print("luas jajar genjang adalah", luas_j)
+
+
+'''Program Dinamis'''
+
+print("\n1.Luas Persegi")
+sisi = int(input("masukan nilai sisi: "))
+luas_p = sisi * sisi
+print("luas persegi adalah", luas_p)
+
+
+
