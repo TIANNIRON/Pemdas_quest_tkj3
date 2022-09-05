26d74b254074a83a622b94d3210e0530bec9a0be
TIANNIRON <112367756+TIANNIRON@users.noreply.github.com>
2022-09-04 21:57:09 -0700

    Add files via upload

---
+++ pyton6-tkj3.py
@@ -0,0 +1,13 @@
+dolar=int(input("masukan nilai dolar:" ))
+ni1I_r=dolar * 14973,8
+print
+
+
+euro=int(input("masukan nilai euro:" ))
+ni1I_w=euro * 1359,22
+print
+
+
+yen=int(input("masukan nilai yen:" ))
+ni1I_r=yen * 106,25
+print
