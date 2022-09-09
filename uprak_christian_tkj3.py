5bbc2d0664103fee5af041838c6e2f6afd3fe96f
TIANNIRON <112367756+TIANNIRON@users.noreply.github.com>
2022-09-08 19:41:30 -0700

    Add files via upload

---
+++ uprak_christian_tkj3.py
@@ -0,0 +1,50 @@
+#balok
+
+panjang=20
+lebar=10
+tinggi=10
+luas_B=panjang*lebar*tinggi
+
+print("luas blok adalah",luas_B)
+
+
+#kubus
+
+sisi=10
+sisi=30
+sisi=20
+luas_K=sisi*sisi*sisi
+
+print("luas kubus adalah",luas_K)
+
+
+#limas
+
+luas=20
+tinggi=10
+luas_L= 1/3*luas*sisi
+
+print("luas limas adalah",luas_L)
+
+
+#tabung
+
+ruang1=10
+ruang2=20
+tinggi=30
+luas_T=22/7*ruang1*ruang2*tinggi
+
+print("luas tabung adalah",luas_T)
+
+
+#SINTAK
+
+
+celcius=int(input("masukan nilai celcius"))
+nilai_R=4/5
+print
+
+
+
+
+
